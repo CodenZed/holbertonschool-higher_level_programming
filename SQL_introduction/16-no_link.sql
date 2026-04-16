37262d62b8375cd77990c82cadc95891e1f9853c
ZED <92740068+CodenZed@users.noreply.github.com>
2026-04-16 11:39:45 +0400

    Create 16-no_link.sql

---
+++ SQL_introduction/16-no_link.sql
@@ -0,0 +1,8 @@
+-- Lists all records of the table second_table
+-- Don't list rows where the name column is empty
+-- Results should display the score and the name (in this order)
+-- Records should be listed by descending score
+SELECT score, name 
+FROM second_table 
+WHERE name IS NOT NULL AND name != ''
+ORDER BY score DESC;
